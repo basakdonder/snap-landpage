6dc3da0eb16fddff3e0c441e212a8f727610c658
Başak Dönder <basakdonder01@gmail.com>
2022-05-09 01:01:41 +0300

    .

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,39 +1,45 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "../../img/logo.svg";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
-import burgerMenu from "./burger-menu";
+import menu from "../../img/icon-menu.svg";
 
-const Navbar = () => {
-  return (
-    <div className="navbar">
-      <div className="left-links">
-        <Link to="/">
-          <img id="logo" src={logo} alt="snap" />
-        </Link>
+export default class Navbar extends Component {
+  state = {
+    clicked: false,
+  };
 
-        <nav id="nav">
-          <Link className="nav-links" to="/features">
-            Features
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked });
+  };
+
+  render() {
+    return (
+      <div className="navbar">
+        <div className="left-links">
+          <Link to="/">
+            <img id="logo" src={logo} alt="snap" />
           </Link>
-          <Link className="nav-links" to="/company">
-            Company
-          </Link>
-          <Link className="nav-links" to="/careers">
-            Careers
-          </Link>
-          <Link className="nav-links" to="/about">
-            About
-          </Link>
-        </nav>
+
+          <nav className={this.state.clicked ? "nav active" : "nav"}>
+            <Link className="nav-links" to="/features">
+              Features
+            </Link>
+            <Link className="nav-links" to="/company">
+              Company
+            </Link>
+            <Link className="nav-links" to="/careers">
+              Careers
+            </Link>
+            <Link className="nav-links" to="/about">
+              About
+            </Link>
+          </nav>
+        </div>
+        <div className="hamburger-menu" onClick={this.handleClick}>
+          <img src={menu} alt="" />
+        </div>
       </div>
-      <div id="hamburger-menu">
-        <span className="bar"></span>
-        <span className="bar"></span>
-        <span className="bar"></span>
-      </div>
-    </div>
-  );
-};
-
-export default Navbar;
+    );
+  }
+}
--- src/components/Navbar/burger-menu.js
+++
@@ -1,9 +0,0 @@
-export default function () {
-  const Menu = document.getElementById("hamburger-menu");
-  const Slider = document.getElementById("nav");
-
-  Menu.addEventListener("click", function () {
-    Menu.classList.toggle("clicked");
-    Slider.classList.toggle("active");
-  });
-}
